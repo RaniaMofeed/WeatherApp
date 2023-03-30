c1764e592617c7fba5b2e103aa26f4c4a1d7a376
Rania Mofeed <RaniaMofeed@gmail.com>
2023-03-30 19:58:05 +0200

    firs Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Building command-line interfaces using argparse
+# Reading public data from the Internet with APIs
+# Integrating APIs in Python code
+# Working with JSON data
+# Importing modules and libraries
+# Defining your own functions
+# Handling errors with Python exceptions
+# Formatting your text using f-strings
+# Using conditional expressions
+#API Link = https://api.openweathermap.org/data/2.5/weather?q=vienna&units=metric&appid=c37e958a915320accf847baee69c8a48
+#Step 1: Get Access to a Suitable Weather API
+import weather
+weather.hellow()
